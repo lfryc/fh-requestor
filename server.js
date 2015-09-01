85105eb13a0b18a98ce42d78458de405795758c2
Lukas Fryc <lukas@fryc.eu>
2015-09-01 12:12:05 +0200

    allow to specify ip/host_header - avoid needs for nginx rule

--- server.js
+++ server.js
@@ -4,40 +4,24 @@ var mbaasExpress = mbaasApi.mbaasExpress();
 var cors = require('cors');
 var request = require('request');
 
-// list the endpoints which you want to make securable here
-var securableEndpoints;
-// fhlint-begin: securable-endpoints
-securableEndpoints = [];
-// fhlint-end
-
 var app = express();
 
-// TODO: Cors is disabled as of now, since it prevents /try route to proxy simply (see #3)
-//app.use(cors());
-
-// Note: the order which we add middleware to Express here is important!
-//app.use('/sys', mbaasExpress.sys(securableEndpoints));
-//app.use('/mbaas', mbaasExpress.mbaas);
-
-// allow serving of static files from the public directory
-//app.use(express['static'](__dirname + '/public'));
-
-// Note: important that this is added just before your own Routes
-//app.use(mbaasExpress.fhmiddleware());
-
-// fhlint-begin: custom-routes
-
 var conf = {
-    host: 'https://testing.feedhenry.me'
+    ip: '192.168.33.10',
+    host_header: 'testing.feedhenry.me'
 };
 
 app.all('/configure', function( req, resp ) {
-    conf.host = req.query['host'];
+    conf.ip = req.query['ip'];
+    conf.host_header = req.query['host_header'];
     resp.status(200).json(conf);
 });
 
 app.all('/*', function( req, resp ) {
-    req.pipe( request(  conf.host + req.originalUrl ) ).pipe( resp );
+    req.pipe( request(  {
+        url: 'https://' + conf.ip + req.originalUrl,
+        headers: { 'Host': conf.host_header  }
+    } ) ).pipe( resp );
 });
 // fhlint-end
 
